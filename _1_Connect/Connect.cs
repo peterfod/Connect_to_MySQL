a7b559fb82d29141154e8d697101ad954fb8c5b5
peter <peterfod@freemail.hu>
2023-10-21 23:12:01 +0200

    Add project files.

---
+++ _1_Connect/Connect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace _1_Connect
+{
+	public class EmployeesTable
+	{
+		public int id { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public string email { get; set; }
+	}
+
+	class Connect
+	{
+		static void Main(string[] args)
+		{
+			List<EmployeesTable> employees = new List<EmployeesTable>();
+			string connString = "server=localhost;database=classicmodels;uid=teszt;pwd=abc123";
+			MySqlConnection conn = new MySqlConnection(connString);
+			Console.WriteLine("Kapcsolat megnyitása...");
+			conn.Open();
+			Console.WriteLine("Kapcsolat megnyitva");
+			string query = "SELECT * FROM employees";
+			MySqlCommand command = new MySqlCommand(query, conn);
+			MySqlDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				EmployeesTable employee = new EmployeesTable
+				{
+					id = Convert.ToInt32(reader["employeeNumber"]),
+					firstName = reader["firstName"].ToString(),
+					lastName = reader["lastName"].ToString(),
+					email = reader["email"].ToString()
+				};
+				employees.Add(employee);
+			}
+
+			reader.Close();
+			conn.Close();
+
+			Console.WriteLine("SQL-ből kiolvasott adatok:");
+
+			foreach (var item in employees)
+			{
+				Console.WriteLine($"\t{item.id} | {item.firstName} | {item.lastName} | {item.email}");
+			}
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
