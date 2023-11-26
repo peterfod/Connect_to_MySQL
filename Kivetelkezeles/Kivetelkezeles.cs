ee8a288d979c8ef332b93c3e13175316f5fd3fef
peter <petya.fodor@gmail.com>
2023-11-26 15:33:52 +0100

    Add new project

---
+++ Kivetelkezeles/Kivetelkezeles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelkezeles
+{
+	class Kivetelkezeles
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
